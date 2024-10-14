1bb40d234ff7f56a4cdd7c5c89071f67cb69d44f
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2024-10-14 12:22:58 -0400

    Backend/API

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.listen(5000, () => {
-  console.log("Server started at https://localhost:5000");
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+app.listen(5001, () => {
+  connectDB();
+  console.log("Server started at https://localhost:" + PORT);
 });

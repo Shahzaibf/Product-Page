119439354897d07c7bc6b842ad34db7e0e342097
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2024-10-17 13:11:31 -0400

    Product edit/delete

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,6 +1,7 @@
-import { Box, Input, Button, Container, Heading, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Box, Input, Button, Container, Heading, useColorModeValue, VStack, useToast } from '@chakra-ui/react';
 import React from 'react'
 import { useState } from 'react'
+import { useProductStore } from '../store/product';
 
 const CreatePage = () => {
     const [newProduct, setNewProduct] = useState({
@@ -8,8 +9,26 @@ const CreatePage = () => {
         price: "",
         image:  ""
     });
-    const handleAddProduct = () => {
-        console.log(newProduct);
+    const toast = useToast();
+    const {createProduct} = useProductStore();
+    const handleAddProduct = async () => {
+        const {success, message} = await createProduct(newProduct)
+        if(!success) {
+            toast({
+                "title":"Error",
+                description: message,
+                status: "error",
+                isClosable: true,
+            })
+        } else {
+            toast({
+                "title":"Success",
+                description: message,
+                status: "success",
+                isClosable: true,
+            })
+        }
+        setNewProduct({ name: "", price: "", image: ""})
     }
   return (
     <Container maxW={"container.sm"}>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,57 @@
+import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import ProductCard from '../components/ProductCard'
+import { useProductStore } from '../store/product'
 
 const HomePage = () => {
+  const {fetchProducts, products} = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
   return (
-    <div>HomePage</div>
+    <Container maxW={'container.xl'} py= {12}>
+      <VStack spacing={8}>
+      <Text
+        bgGradient='linear(to-l, cyan.400, blue.500)'
+        bgClip='text'
+        textAlign={"center"}
+        textTransform={"uppercase"}
+        fontSize={{base: "22", sm: "28"}}
+        fontWeight='bold'
+            >
+              Current Products
+          </Text>
+          
+          <SimpleGrid
+          column={{
+            base: 1,
+            md: 2,
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}
+          >
+            {products.map((product) => (
+              <ProductCard key={product._id} product={product}/>
+            ))}
+          </SimpleGrid>
+
+          {products.length === 0 && (
+            <>
+              <Text fontSize={'xl'} textAlign={'center'} fontWeight={'bold'} color={'gray.500'}>
+              No products found
+            </Text>
+            <Link to={'/create'}>
+              <Text as={'span'} color={'blue.500'} _hover={{ textDecoration: "underline"}}>
+                Create a product
+              </Text>
+            </Link>
+            </>
+          )}
+      </VStack>
+    </Container>
   )
 }
 
